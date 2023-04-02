0b71b87769bfbcce26e572ecd4a40049e8aaf513
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-02 19:20:50 +0300

    remove logs from request listener

--- req-listener.js
+++ req-listener.js
@@ -2,7 +2,6 @@ module.exports = {handleRequest}
 
 function handleRequest(mongo) {
   return async function handleRequest(request, response) {
-    console.log(request.url, response.toString())
     response.end('hi there!')
   }
 }
