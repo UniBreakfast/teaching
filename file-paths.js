42ceb44410913c228e8db70f97fa15b821619255
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-03 00:24:28 +0300

    Update file-paths.js

--- file-paths.js
+++ file-paths.js
@@ -3,9 +3,11 @@ module.exports = {listFilePaths}
 async function listFilePaths(path = '.') {
   const ents = await readdir(path, { withFileTypes: true })
   const paths = await Promise.all(ents.map(ent => {
-    const res = resolve(path, ent.name)
-    console.log(ent.name)
-    if (ent.name == 'node_modules' || ent.name == '.git') return 
+    const {name} = ent
+    const res = resolve(path, name)
+    console.log(name)
+    if (name == 'node_modules' || name == '.git') return 
+    if (!name.match(/\.js$/) && name != 'package.json') return 
     
     return ent.isFile() ? res : listFilePaths(res)
   }));
