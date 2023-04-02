8d95c50f61368e005461abf5b36f3e4346fe28bc
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-02 14:26:30 +0300

    wait for insertion

--- go-mongo.js
+++ go-mongo.js
@@ -2,8 +2,8 @@
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = process.env.MONGO_CONN_STR
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-client.connect().then(() => {
+client.connect().then(async () => {
   const studentsCollection = client.db("teaching").collection("students")
-  studentsCollection.insertOne({name: 'Yana Vozniuk', nCode: true})
+  await studentsCollection.insertOne({name: 'Yana Vozniuk', nCode: true})
   client.close()
 }).catch(console.log)
