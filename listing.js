37732d7852b915a3ef24a5c04dcebc3686cc07f1
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-02 21:55:48 +0300

    Create listing.js

---
+++ listing.js
@@ -0,0 +1,16 @@
+module.exports = {buildFileListing}
+
+async function builFileListing() {
+  const paths = await listFilePaths()
+  const contents = await Promise.all(paths.map(readFile))
+  
+  let listing = ''
+  
+  for (let i = 0; i < paths.length; i++) {
+    listing += `${path}\n\`\`\`\n${content}\n\`\`\`\n\n`
+  }
+  
+  return listing
+}
+
+const {readFile} = require('fs/promises')
