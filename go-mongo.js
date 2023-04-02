0511060d39e3d34e1666511785f6ffc0ec4b97a0
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-02 19:28:14 +0300

    Delete go-mongo.js

--- go-mongo.js
+++
@@ -1,9 +0,0 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri = process.env.MONGO_CONN_STR
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
-client.connect().then(async () => {
-  const studentsCollection = client.db("teaching").collection("students")
-  await studentsCollection.insertOne({name: 'Yana Vozniuk', nCode: true})
-  client.close()
-}).catch(console.log)
