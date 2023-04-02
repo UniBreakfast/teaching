06233079aadb2273457cbbdb90993db19f166466
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-02 17:38:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 module.exports = {prepareHttpServer}
 
 async function prepareHttpServer() {
+  const server = createServer()
 
+  return new Promise((resolve, reject) => {
+    server.on('error', reject)
+
+    try {
+      server.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`)
+        resolve(server)
+      })
+    } catch (err) {
+      reject(err)
+    }
+  })
 }
 
+const port = process.env.PORT
 const {createServer} = require('http')
