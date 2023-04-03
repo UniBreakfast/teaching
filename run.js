d974130dcf700e73e947e1cceb8a4a9f0cc4f3d8
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-03 06:22:32 +0300

    Update run.js

--- run.js
+++ run.js
@@ -2,6 +2,8 @@ module.exports = {run}
 
 async function run({server, mongo}) {
   server.on('request', handleRequest(mongo))
+  
+  console.log('request handler is listening...')
 }
 
 const {handleRequest} = require('./req-listener.js')
