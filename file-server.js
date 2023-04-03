aceebfa7e6e87da975ddce31de80175ecc540850
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-03 10:47:51 +0300

    Update file-server.js

--- file-server.js
+++ file-server.js
@@ -1,5 +1,14 @@
 module.exports = {serveFile}
 
-async function serveFile(request, response) {
-  response.end('no files are served yet')
+async function serveFile(request, response, fileCache) {
+  const path = request.url.slice(1) || "index.html"
+  
+  if (path in fileCache) {
+    const fileContent = fileCache[path]
+    
+    response.end(fileContent)
+  }
+  else {
+    response.end('File not found: ' + path)
+  }
 }
