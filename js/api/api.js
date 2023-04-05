01f98fc8f95ba8512207d5094571026508ea64cb
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-05 14:32:55 +0300

    add logs for students

--- js/api/api.js
+++ js/api/api.js
@@ -3,10 +3,14 @@ module.exports = {handleAPI}
 async function handleAPI(request, response, mongo) {
   const endpoint = request.url.slice(5)
 
+  console.log('endpoint requested: ' + endpoint)
+
   if (endpoint == 'students') {
     const studentsCollection = mongo.db('teaching').collection('students')
     const students = await studentsCollection.find().toArray()
     
+    console.log(students.length + ' student records found')
+
     response.setHeader('Content-Type', 'application/json')
     response.end(JSON.stringify(students))
   } else
