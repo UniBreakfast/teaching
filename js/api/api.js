0e9117d7785fe90e38e6963784152c36b981b44a
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-05 14:24:49 +0300

    branch api

--- js/api/api.js
+++ js/api/api.js
@@ -1,9 +1,24 @@
 module.exports = {handleAPI}
 
 async function handleAPI(request, response, mongo) {
-  const listing = await buildFileListing()
-  console.log(`File listing ${listing.length} characters long is sent`)
-  response.end(listing)
+  const endpoint = request.url.slice(5)
+
+  if (endpoint == 'students') {
+    const studentsCollection = mongo.db('teaching').collection('students')
+    const students = await studentsCollection.find().toArray()
+    
+    response.setHeader('Content-Type', 'application/json')
+    response.end(JSON.stringify(students))
+  } else
+  if (endpoint == 'listing') {
+    const listing = await buildFileListing()
+    
+    console.log(`File listing ${listing.length} characters long is sent`)
+    response.end(listing)
+  }
+  else {
+    response.end('no point to request ' + endpoint)
+  }
 }
 
 const {buildFileListing} = require('../file-work/listing.js')
