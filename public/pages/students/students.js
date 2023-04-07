9da74701e3f8498e1e060b56854e99772c17a47c
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-07 08:15:43 +0300

    remove student btn

--- public/pages/students/students.js
+++ public/pages/students/students.js
@@ -2,6 +2,28 @@ const studentsList = document.getElementById('students')
 
 getStudents().then(showStudents)
 
+studentsList.onclick = async e => {
+  if (e.target.innerHTML = 'remove') {
+    const btn = e.target
+    const li = btn.closest('li')
+    const id = li.dataset.id
+    const result = await removeStudent(id)
+    
+    btn.disabled = true 
+    
+    if (result.success) li.remove
+    else btn.disabled = false
+  }
+}
+
+function removeStudent(id) {
+  const init = {method: 'DELETE', body: JSON.stringify({id})
+  const response = await fetch('/api/student', init)
+  const result = await response.json()
+  
+  return result 
+}
+
 async function getStudents() {
   const response = await fetch('/api/students')
   const students = await response.json()
@@ -14,11 +36,11 @@ function showStudents(students) {
 }
 
 function buildStudentItem(student) {
-  const {name} = student
+  const {name, _id} = student
 
   return `
-    <li>
-      ${name}
+    <li data-id="_id">
+      ${name} <button>remove</button>
     </li>
   `
 }
