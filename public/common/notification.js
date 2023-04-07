e79110b8c50cfc711f39b014979819155c71576f
Mykhailo "Ninin" Velykoselskyi <unibreakfast1@gmail.com>
2023-04-07 14:09:48 +0300

    Update notification.js

--- public/common/notification.js
+++ public/common/notification.js
@@ -2,4 +2,35 @@ export {notify}
 
 async function notify(...items) {
   const card = createCard(...items)
+  
+  card.onclick = e => {
+    if (e.target.innerHTML == '&times;') card.remove()
+  }
+  
+  body.append(card)
 }
+
+function createCard(...items) {
+  const card = document.createElement('div')
+  
+  card.innerHTML = `
+    <div style="${cardStyle}>
+      <b>&times;</b><br>
+      <ul>
+        ${items.map(item => `<li>${item}</li>`).join('')}
+      </ul>
+    </div>
+  `
+  
+  return card
+}
+
+const cardStyle = `
+  position: fixed;
+  right: 10px;
+  bottom: 10ps;
+  width: 500px;
+  max-width: 90vw;
+  padding: 5px;
+  white-space: pre;
+`
